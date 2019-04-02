62d13f196530e369b23fd217628ffeae64d37ebd
EMILIO <emilioesteve99@icloud.com>
2019-04-02 19:01:13 +0200

    First Commit

---
+++ WebApplication1/Website/Professor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website
+{
+    public class Professor
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Professor(int id, string name )
+        {
+
+            this.Id = id;
+            this.Name = name;
+
+        }
+
+    }
+}
